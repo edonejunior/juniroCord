b9f4d3c9ac049fc1586e8a48fd7e7ee968c9a8be
Edvaldo Domingues de Faria Junior <edonejunior@hotmail.com>
2022-01-25 16:49:58 -0300

    Desafio - Validar caracteres

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,11 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import {useRouter} from 'next/router'
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
+
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -46,25 +23,16 @@ function Titulo(props) {
   );
 }
 
-// Componente React
-// function HomePage() {
-//     // JSX
-//     return (
-//         <div>
-//             <GlobalStyle />
-//             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-//             <h2>Discord - Alura Matrix</h2>
-//         </div>
-//     )
-// }
-// export default HomePage
-
 export default function PaginaInicial() {
-  const username = 'peas';
+
+  const [username, setUsername] = React.useState('')
+  const [contaletra, setContaletra] = React.useState('')
+  const roteamento = useRouter();
+
 
   return (
     <>
-      <GlobalStyle />
+      
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +59,11 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function(event){
+              event.preventDefault();
+              //Roteamento Next
+              roteamento.push('/chat');
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -102,6 +75,16 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              value={contaletra}
+              onChange={(event) => {
+                setContaletra(event.target.value);
+                const valor = event.target.value;
+                if (valor.length > 2){
+                  setUsername(valor);
+                }
+                
+              }
+                }
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -148,7 +131,7 @@ export default function PaginaInicial() {
                 borderRadius: '50%',
                 marginBottom: '16px',
               }}
-              src={`https://github.com/${'edonejunior'}.png`}
+              src={`https://github.com/${username}.png`}
             />
             <Text
               variant="body4"
@@ -159,7 +142,7 @@ export default function PaginaInicial() {
                 borderRadius: '1000px'
               }}
             >
-              {'edonejunior'}
+              {username}
             </Text>
           </Box>
           {/* Photo Area */}
