973f9b43e2a684b47e700c0f4ad41adb871bd33d
Edvaldo Domingues de Faria Junior <edonejunior@hotmail.com>
2022-01-25 10:06:01 -0300

    Color | Background

--- pages/index.js
+++ pages/index.js
@@ -68,8 +68,8 @@ export default function PaginaInicial() {
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          backgroundColor: appConfig.theme.colors.primary['700'],
+          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/07/super-mario-bros-level-ending.jpg)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
       >
